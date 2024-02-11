81e2b6500145cbdac660abd049c2b7a9fca165b4
nopeg <vadimus.05ev@mail.ru>
2024-02-12 00:56:46 +0300

    cmake changes and device enumeration

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
+
+#include <AL/al.h>
+#include <AL/alc.h>
+
 #include "testHeader.h"
-#include "AL/al.h"
+
+static void list_audio_devices(const ALCchar* devices)
+{
+    const ALCchar* device = devices, * next = devices + 1;
+    size_t len = 0;
+
+    std::cout << "Devices list:\n";
+    std::cout << "----------\n";
+    while (device && *device != '\0' && next && *next != '\0') 
+    {
+        std::cout << "%s\n" << device;
+        len = strlen(device);
+        device += (len + 1);
+        next += (len + 2);
+    }
+    std::cout << "----------\n";
+}
 
 int main(int argc, char *argv[], char *envp[])
 {
+	ALboolean enumeration;
+
+	enumeration = alcIsExtensionPresent(NULL, "ALC_ENUMERATION_EXT");
+	if (enumeration == AL_FALSE)
+	{
+        std::cout << "no enumeration\n";
+	}
+	else
+	{
+        list_audio_devices(alcGetString(NULL, ALC_DEVICE_SPECIFIER));
+	}
 	func();
 	return 0;
 }
